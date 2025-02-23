c7b2ebd719babb0b74ecfd4e2cd696b574203da1
mik-23 <159887945+Mik-23@users.noreply.github.com>
2025-02-23 12:13:03 +0300

    Update settings.py

--- porno_chrome/settings.py
+++ porno_chrome/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '158.160.130.157']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '158.160.149.238']
 
 
 # Application definition
