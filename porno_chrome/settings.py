9984ec208b92fe05a409806ce42694f41ec10e8a
mik-23 <159887945+Mik-23@users.noreply.github.com>
2025-03-02 02:10:21 +0300

    Update settings.py

--- porno_chrome/settings.py
+++ porno_chrome/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "main/static", BASE_DIR / "search/static"
+    BASE_DIR / "main/static_v1", BASE_DIR / "search/static_v2"
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
