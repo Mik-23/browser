77cf0f015abb54f6bada9aad81d2e758ad356875
mik-23 <159887945+Mik-23@users.noreply.github.com>
2025-03-11 20:02:50 +0300

    Update settings.py

--- porno_chrome/settings.py
+++ porno_chrome/settings.py
@@ -130,3 +130,4 @@ STATIC_ROOT = os.path.join('staticfiles')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
