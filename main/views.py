e316588aa2d4c7f30a6539ebb78333fa74be8e7d
mik-23 <159887945+Mik-23@users.noreply.github.com>
2025-04-20 14:01:11 +0300

    Update views.py

--- main/views.py
+++ main/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def site(request):
     return render(request, 'main/site.html', {'title': 'MixRech'})
+
+
+def account(request):
+    return render(request, 'main/account.html')
