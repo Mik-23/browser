0fc29bc7a96a0e38653104f7cc0c6ed743a473be
mik-23 <159887945+Mik-23@users.noreply.github.com>
2025-05-05 02:53:19 +0300

    Delete main directory

--- main/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views, api
-
-urlpatterns = [
-    path('', views.site, name='home'),
-    path('account', views.account, name='account'),
-    path('api/register/', api.RegisterView.as_view() , name='register'),
-    path('api/login/', api.LoginView.as_view() , name='login')
-]
