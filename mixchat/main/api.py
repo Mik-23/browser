733808b1d9785a0774f086fa9645a611bbc5bbff
mik-23 <159887945+Mik-23@users.noreply.github.com>
2025-06-06 17:57:05 +0300

    Update api.py

--- mixchat/main/api.py
+++ mixchat/main/api.py
@@ -1,9 +1,12 @@
 from rest_framework import generics
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from django.db.models import Q
+from django.urls import reverse
+from django.http import HttpResponse
 from django.contrib.auth.models import User
 from .models import Message, Chat, Channel, ChannelMembership
 from .serialazers import (UserSerializer, LoginSerializer, SendMessageSerializer, SearchUserSerializer,
@@ -15,6 +18,17 @@ from .serialazers import (UserSerializer, LoginSerializer, SendMessageSerializer
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+
+        # Возвращаем сообщение об успешной регистрации и URL для авторизации
+        return Response({
+            'message': 'Пользователь успешно зарегистрирован.',
+            'login_url': request.build_absolute_uri(reverse('successfully'))  # Убедитесь, что у вас есть имя URL для страницы авторизации
+        })
 
 
 class LoginView(generics.GenericAPIView):
@@ -34,28 +48,82 @@ class LoginView(generics.GenericAPIView):
         })
 
 
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        print(request.data)
+        # Получаем токен из заголовка Authorization
+        #try:
+        #    refresh_token = request.data["refresh"]
+        #    tok = RefreshToken(refresh_token)
+        #    tok.blacklist()
+        #except Exception as e:
+        #    print(e)
+        #    return Response(status=400, data={"detail": "Invalid token"})
+
+        return Response({"detail": "Successfully logged out.",
+                         'logout_redirect': '/auth_in_chat'})
+
+
 class SendMessageView(generics.GenericAPIView):
+    # Отправить сообщение
     serializer_class = SendMessageSerializer
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
 
     def post(self, request, *args, **kwargs):
+        print(request.user.id)
         serializer = self.get_serializer(data=request.data)
+        print(request.data)
         serializer.is_valid(raise_exception=True)
         sender_id = request.user.id
         recipient_id = serializer.validated_data['recipient_id']
+        print('Отправитель', sender_id)
+        print('Получатель', recipient_id)
         content = serializer.validated_data['content']
         chat_id = serializer.validated_data['chat_id']
-        print('ЧАТТТТТ', chat_id)
-        message = Message.objects.create(sender_id=sender_id, recipient_id=recipient_id, content=content, chat_id=chat_id)
-        return Response({
+        print('ID чата', chat_id)
+        image_file = request.FILES.get('image')
+        video_file = request.FILES.get('video')
+        audio_file = request.FILES.get('audio')
+        print('VIDEO', video_file)
+        message = Message(
+            sender_id=sender_id,
+            content=content,
+            recipient_id=recipient_id,
+            chat_id=chat_id,
+        )
+        if image_file:
+            message.image.save(image_file.name, image_file)
+        else:
+            message.image = None
+
+        if video_file:
+            message.video.save(video_file.name, video_file)
+        else:
+            message.video = None
+
+        if audio_file:
+            message.audio.save(audio_file.name, audio_file)
+        else:
+            message.audio = None
+
+        message.save()
+        print(message)
+        return HttpResponse({
             'sender_id': sender_id,
             'recipient_id': recipient_id,
-            'content': content
+            'content': content,
+            'image': image_file,
+            'video': video_file,
+            'audio': audio_file,
+            'message_id': message.id
         })
 
 
 class SearchUserView(generics.GenericAPIView):
+    # Поиск по пользователю
     serializer_class = SearchUserSerializer
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
@@ -74,6 +142,7 @@ class SearchUserView(generics.GenericAPIView):
 
 
 class GetOneChatView(generics.GenericAPIView):
+    # Открыть один чат
     serializer_class = GetOneChatSerializer
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
@@ -84,7 +153,6 @@ class GetOneChatView(generics.GenericAPIView):
 
         # Упорядочиваем ID пользователей
         user_ids = sorted([sender_id, recipient_id])
-
         # Логика для поиска существующего чата
         chat = Chat.objects.filter(
             user_id_1=user_ids[0],
@@ -98,6 +166,7 @@ class GetOneChatView(generics.GenericAPIView):
 
 
 class GetChatsView(generics.GenericAPIView):
+    # Показать все чаты
     serializer_class = GetChatsSerializer
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
@@ -105,10 +174,20 @@ class GetChatsView(generics.GenericAPIView):
     def get(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        chats = Chat.objects.all()
+        chats = Chat.objects.filter(
+        Q(user_id_1=request.user.id) |
+        Q(user_id_2=request.user.id))
         users = User.objects.all()
+        print(chats)
         chat_with_user = []
-        for user, chat in zip(users, chats):
+        for user, chat in zip(users, sorted(chats, key=lambda user: user.user_id_2)):
+            print(f'{user}      {chat}')
+            if chat.user_id_1 < request.user.id and chat.user_id_2 == request.user.id:
+                user = User.objects.filter(id=chat.user_id_1).first()
+            elif chat.user_id_1 == request.user.id and chat.user_id_2 > request.user.id:
+                user = User.objects.filter(id=chat.user_id_2).first()
+            elif chat.user_id_1 == chat.user_id_2 == request.user.id:
+                user = User.objects.filter(id=chat.user_id_1).first()
             chat_with_user.append({
                 "id": chat.id,
                 "user_id_1": chat.user_id_1,
@@ -121,6 +200,7 @@ class GetChatsView(generics.GenericAPIView):
 
 
 class GreateChatView(generics.GenericAPIView):
+    # Создать чат
     serializer_class = CreateChatSerializer
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
@@ -141,6 +221,7 @@ class GreateChatView(generics.GenericAPIView):
 
 
 class MessageView(generics.GenericAPIView):
+    # Показать все сообщения в чате
     serializer_class = MessageSerializer
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
@@ -154,6 +235,9 @@ class MessageView(generics.GenericAPIView):
                 "sender_id": str(message.sender),
                 "recipient_id": str(message.recipient),
                 "content": message.content,
+                "image": message.image.url if message.image else None,
+                "video": message.video.url if message.video else None,
+                "audio": message.audio.url if message.audio else None,
                 "date": message.timestamp
             } for message in messages]
         })
