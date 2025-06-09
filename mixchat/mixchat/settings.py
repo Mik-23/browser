8282fbc6ec9a7eca39f1639a405a923c51559cea
mik-23 <159887945+Mik-23@users.noreply.github.com>
2025-06-09 18:03:27 +0300

    Update settings.py

--- mixchat/mixchat/settings.py
+++ mixchat/mixchat/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%@mil*7q^3c-i+4k#@qg2r5(3w9+auty8!@a+$1^nnj^f#m9)*'
+SECRET_KEY = config("SECRET_KEY_MIXCHAT")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
