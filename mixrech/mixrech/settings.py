e8c901b516558de7f18dd5c894db9c14221bced4
mik-23 <159887945+Mik-23@users.noreply.github.com>
2025-06-09 18:15:57 +0300

    Update settings.py

--- mixrech/mixrech/settings.py
+++ mixrech/mixrech/settings.py
@@ -80,12 +80,8 @@ WSGI_APPLICATION = 'mixrech.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config('MIXRECH_DATABASE'),
-        'USER': config('DATABASE_USER'),
-        'PASSWORD': config('DATABASE_PASSWORD'),
-        'HOST': config('DATABASE_HOST'),
-        'PORT': config('DATABASE_PORT'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
