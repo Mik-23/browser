0b5ce8a261c99f3e33a6d6dd4e9bcfb8f5eb1f8a
mik-23 <159887945+Mik-23@users.noreply.github.com>
2025-06-10 03:04:32 +0300

    Update settings.py

--- mixrech/settings.py
+++ mixrech/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '158.160.133.169', '0.0.0.0', 'mixrech.com', 'google.com']
 
