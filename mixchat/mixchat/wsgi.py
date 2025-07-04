423d0168e310219d2dc33bf11c464ce9de12e63d
mik-23 <159887945+Mik-23@users.noreply.github.com>
2025-07-05 01:17:33 +0300

    Update wsgi.py

--- mixchat/mixchat/wsgi.py
+++ mixchat/mixchat/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mixchat.mixchat.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mixchat.settings')
 
 application = get_wsgi_application()
