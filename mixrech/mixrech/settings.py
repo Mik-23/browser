2c80a8f08289a61fe8d14519eb7bacf125dfc627
mik-23 <159887945+Mik-23@users.noreply.github.com>
2025-10-12 23:56:22 +0300

    Update settings.py

--- mixrech/mixrech/settings.py
+++ mixrech/mixrech/settings.py
@@ -12,17 +12,18 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 import os
 from pathlib import Path
-from decouple import config
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv()
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY_MIXRECH')
+SECRET_KEY = os.getenv('SECRET_KEY_MIXRECH')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
