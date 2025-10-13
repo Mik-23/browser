b9798872f06d4f2326cdbda536bab02a87eeab32
mik-23 <159887945+Mik-23@users.noreply.github.com>
2025-10-13 13:20:18 +0300

    Update redirect_url.js

--- mixchat/main/static_v4/main/js/redirect_url.js
+++ mixchat/main/static_v4/main/js/redirect_url.js
@@ -8,13 +8,26 @@ const loginUser = async (email, password) => {
     });
     const data = await response.json();
     console.log(data)
+    console.log(data.redirect_url)
     if (response.ok) {
         // Сохраните токены в localStorage или cookies
-        localStorage.setItem('access_token', data.access);
-        localStorage.setItem('refresh_token', data.refresh);
-        localStorage.setItem('user_id', data.user_id);
-        // Перенаправьте пользователя на страницу почты
-        window.location.href = data.redirect_url;
+        if (data.error != undefined) {
+           const inputs = document.querySelectorAll('.form-group input');
+           inputs.forEach(input => {
+               input.style.border = '2px solid red'; // например, красная граница, толщиной 2px
+           });
+           error = document.getElementById('errorMessage');
+           error.textContent = data.error
+        } else {
+            localStorage.setItem('access_token', data.access);
+            localStorage.setItem('refresh_token', data.refresh);
+            localStorage.setItem('user_id', data.user_id);
+            // Перенаправьте пользователя на страницу почты
+            window.location.href = data.redirect_url;
+            if (data.error) {
+               alert(data.error)
+            }
+        }
     } else {
         // Обработка ошибок
         console.error(data.error);
