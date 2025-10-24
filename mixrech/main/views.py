678e7a8c26d10a5e8bc4a2a715df70a5c6a054e1
mik-23 <159887945+Mik-23@users.noreply.github.com>
2025-10-24 13:13:08 +0300

    Update views.py

--- mixrech/main/views.py
+++ mixrech/main/views.py
@@ -1,6 +1,16 @@
+import os
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.conf import settings
 
 
 # Create your views here.
 def site(request):
     return render(request, 'main/site.html')
+
+
+def text_view(request):
+    file_path = os.path.join(settings.BASE_DIR, 'main', 'robots.txt')
+    print('dfgdgd',file_path)
+    with open (file_path, 'r', encoding='utf-8') as f:
+        return HttpResponse(f.read(), content_type='text/plain; charset=utf-8')
