2e0e49b18a9eb42e9cb35bb076d309eb641c2047
mik-23 <159887945+Mik-23@users.noreply.github.com>
2026-02-19 16:45:28 +0300

    Update urls.py

--- mixrech/search/urls.py
+++ mixrech/search/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import search_view, image_view, video_view, search_by_image_view, news_view
+from .api import VoiceView
 
 urlpatterns = [
     path('', search_view, name='search'),
     path('image', image_view, name='images'),
     path('video', video_view, name='videos'),
     path('search_by_image', search_by_image_view, name='search_by_image'),
-    path('news', news_view, name='news')
+    path('news', news_view, name='news'),
+    path('api/voice', VoiceView.as_view(), name='voice')
 ]
