a2f8d0e6cd3e19309dccfcadb1311d8dbe9b4ac6
mik-23 <159887945+Mik-23@users.noreply.github.com>
2026-03-13 20:51:46 +0300

    Add files via upload

---
+++ mixchat/main/static_v4/main/js/fcm-sw.js
@@ -0,0 +1,25 @@
+import { getMessaging, getToken, deleteToken } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-messaging.js";
+
+const messaging = getMessaging();
+
+getToken(messaging, {vapidKey: 'BISnUtSCVp9xdEpjULSIJVAmSSxDpZyjddQdR7NHlko2tAZNX7apnVL5feKslk1iS71cMQ8xJuH5_lx0O_Yx3UM'}).then((currentToken) => {
+  if (currentToken) {
+    fetch('/api/save_fcm/', {
+       method: 'PUT',
+       headers: {
+           'Content-Type': 'application/json',
+           'Authorization': 'Bearer ' + localStorage.getItem('access_token') // Используйте токен доступа
+       },
+       body: JSON.stringify({"token": currentToken})
+   })
+  } else {
+    // Show permission request UI
+    console.log('No registration token available. Request permission to generate one.');
+    // ...
+  }
+}).catch((err) => {
+  console.log('An error occurred while retrieving token. ', err);
+  // ...
+});
+
+
