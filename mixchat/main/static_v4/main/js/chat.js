4a30176e4945fb71c7adb84e299d74243b17bcfd
mik-23 <159887945+Mik-23@users.noreply.github.com>
2026-03-20 18:22:50 +0300

    Update chat.js

--- mixchat/main/static_v4/main/js/chat.js
+++ mixchat/main/static_v4/main/js/chat.js
@@ -4,6 +4,8 @@ let currentChatId = null;
 let type = null;
 const senderId = localStorage.getItem('user_id');
 let profileId = senderId
+let chatObj = []
+let selectedUserIds = [];
 
 console.log(senderId)
 // Функция для получения пользователей из API
@@ -24,8 +26,6 @@ function fetchUsers() {
     })
     .then(data => {
         users = data.users; // Сохраняем полученных пользователей в переменную
-        //displayUsers(); // Отображаем пользователей
-
     })
     .catch(error => console.error('Ошибка при получении пользователей:', error));
 }
@@ -33,15 +33,41 @@ function fetchUsers() {
 
 // Функция для отображения пользователей
 function displayUsers() {
-    const userList = document.getElementById('users');
+    const userList = document.getElementById('groupUsers');
     userList.innerHTML = ''; // Очистить список
+    console.log(users)
     if (users.length > 0) { // Проверяем, есть ли пользователи
         users.forEach(user => {
             const li = document.createElement('li');
             console.log(user.id)
             console.log(user)
-            //li.textContent = user.username; // Имя пользователя
-            li.onclick = () => selectUser(user.id); // Выбор пользователя
+            const input = document.createElement('input')
+            input.type = 'checkbox'
+            input.value = user.id
+            const img = document.createElement('img')
+            const span = document.createElement('span')
+            img.src = user.photo
+            img.style.width = '20px'
+            img.style.height = '20px'
+            span.textContent = user.username; // Имя пользователя
+            li.appendChild(input)
+            li.appendChild(img)
+            li.appendChild(span)
+            li.onclick = function(e) {
+                 if (e.target.tagName !== 'INPUT') {
+                     const checkbox = this.querySelector('input');
+                     checkbox.checked = !checkbox.checked;
+                 }
+
+                 const checkbox = this.querySelector('input');
+                 if (checkbox.checked) {
+                     this.classList.add('selected');
+                     selectedUserIds.push(user.id);
+                 } else {
+                     this.classList.remove('selected');
+                     selectedUserIds = selectedUserIds.filter(id => id !== user.id);
+                 }
+             };
             userList.appendChild(li);
         });
         userList.style.display = 'block'; // Показываем список
@@ -53,10 +79,7 @@ function displayUsers() {
 // Функция для выбора пользователя
 function selectUser(userId) {
    selectedUserId = userId; // Сохраняем ID выбранного пользователя
-   console.log(selectedUserId)
-   if (selectedUserId) {
-       openChat(selectedUserId);
-   }
+   openUserChat(selectedUserId)
 }
 
 // Функция для поиска пользователей
@@ -96,60 +119,74 @@ document.addEventListener('click', function(event) {
 });
 
 // Функция для открытия чата с пользователем
-function openChat(userId) {
-    const parseUserId = userId;
-    const parseSenderId = senderId;
+function openUserChat(userId) {
     let selectedUser = {}
+    let selected_chat = null
+    const name = null
+    const photo = null
+
     for (user of users) {
-       if(user.id === parseUserId) {
+       if(user.id === userId) {
            selectedUser = user
        }
-   }
-    // Сначала проверяем наличие чата
-    const url = `/api/chat/?sender_id=${parseSenderId}&recipient_id=${parseUserId}`;
+    }
+    for (obj of chatObj) {
+       if(obj.username === selectedUser.username) {
+         selected_chat = obj
+       }
+    }
+    console.log(selected_chat)
+    if (selected_chat) {
+        openChat(selected_chat.id)
+    } else {
+        createChat([senderId, userId], name, photo, selectedUser.type)
+    }
 
-    fetch(url, {
-        method: 'GET',
-        headers: {
-            'Authorization': 'Bearer ' + localStorage.getItem('access_token')
-        }
-    })
-    .then(response => response.json())
-    .then(data => {
-        if (data.chat_id) {
-            // Если чат существует, открываем его
-            currentChatId = data.chat_id;
-            console.log(data)
-            loadMessages(currentChatId);
-        } else {
-            // Если чат не существует, создаем новый
-            console.log(parseSenderId)
-            console.log(parseUserId)
-            createChat(parseSenderId, parseUserId, selectedUser.type);
-        }
-        document.getElementById('chatWindow').style.display = 'block';
+}
 
 
-    })
-    .catch(error => console.error('Ошибка:', error));
+function openChat(currentChatId) {
+     loadMessages(currentChatId);
+     document.getElementById('chatWindow').style.display = 'block';
 }
 
 document.querySelector('.user-name img').addEventListener('click', () => {
-
+    console.log(chatObj)
     const name = document.querySelector('.user-name p').textContent
-    for (user of users){
+    let current_chat = {}
+    for (chat of chatObj) {
+        if (chat.username === name) {
+            current_chat = chat
+        }
+    }
+    for (user of users) {
+        console.log(user)
         if (user.username === name && user.type === 'user') {
             profileId = user.id
         }
     }
-    getProfile()
-    const profileForm = document.getElementById('profileForm');
-    profileForm.style.display = 'block';
-    document.querySelector('.save-btn').style.display = 'none'
-    document.getElementById('photo_input').style.display = 'none'
-    document.getElementById('name_input').disabled = 1
-    document.getElementById('date_birth_input').disabled = 1
-    document.getElementById('bio_input').disabled = 1
+    if (current_chat.type === 'user') {
+        getProfile()
+        const profileForm = document.getElementById('profileForm');
+        profileForm.style.display = 'block';
+        document.querySelector('.save-btn').style.display = 'none'
+        document.getElementById('photo_input').style.display = 'none'
+        document.getElementById('name_input').disabled = 1
+        document.getElementById('date_birth_input').disabled = 1
+        document.getElementById('bio_input').disabled = 1
+    } else if (current_chat.type === 'group') {
+        const profileGroup = document.getElementById('profileGroup');
+        profileGroup.style.display = 'block';
+        chatUserView(current_chat.id)
+        document.querySelector('.save-group').style.display = 'none'
+        document.querySelector('.edit-group').style.display = 'none'
+        document.querySelector('.profile-group-form img').src = current_chat.photo;
+        document.getElementById('name_group_input').value = current_chat.username
+        document.getElementById('bio_group_input').value = current_chat.bio
+        document.getElementById('photo_group_input').style.display = 'none'
+        document.getElementById('name_group_input').disabled = 1
+        document.getElementById('bio_group_input').disabled = 1
+    }
 });
 
 document.getElementById('profile').addEventListener('click', () => {
@@ -157,6 +194,12 @@ document.getElementById('profile').addEventListener('click', () => {
     profileId = senderId
     getProfile()
     profileForm.style.display = 'block';
+});
+
+document.getElementById('group-create').addEventListener('click', () => {
+    const profileGroup = document.getElementById('profileGroup');
+    profileGroup.style.display = 'block';
+    displayUsers()
 });
 
 function getCurrentUser() {
@@ -252,22 +295,26 @@ function loadMessages(chatId) {
                 const bgColor = window.getComputedStyle(li).backgroundColor;
                 return bgColor === 'rgb(31, 148, 148)';
             });
-        const current_img = targetElement.querySelector('img').src
-        const current_p = targetElement.querySelector('span').textContent
+        console.log(data)
         const img = document.querySelector('.user-name img')
         const p = document.querySelector('.user-name p')
-        p.textContent = current_p
-        p.style.color = '#D9FFFD'
-        img.src = current_img
-        img.style.width = '50px'
-        img.style.height = '50px'
+        if (targetElement) {
+            const current_img = targetElement.querySelector('img').src
+            const current_p = targetElement.querySelector('span').textContent
+            p.textContent = current_p
+            p.style.color = '#D9FFFD'
+            img.src = current_img
+            img.style.width = '50px'
+            img.style.height = '50px'
+        } else {
+        }
         userName.appendChild(img)
         userName.appendChild(p)
         data.messages.forEach(message => {
             const li = document.createElement('li');
             const messageContainer = document.createElement('div');
 
-            if (message.sender_id === senderId) {
+            if (message.sender_user_id === senderId) {
                 li.style.backgroundColor = '#1F9494'
                 li.style.alignSelf = 'flex-end';
                 li.style.marginLeft = 'auto';
@@ -279,11 +326,14 @@ function loadMessages(chatId) {
             if (message.content) {
                 if (message.content.includes("https://")) {
                     const textSpan = document.createElement('span');
-                    textSpan.textContent = message.sender;
+                    console.log(message.sender_bot)
+                    textSpan.textContent = message.sender_bot;
                     textSpan.style.maxWidth = '300px';
                     textSpan.style.wordWrap = 'break-word';
                     console.log(message.content)
-                    const jsonString = message.content.replace(/'/g, '"');
+                    let jsonString = message.content.replace(/"/g, '#');
+                    jsonString = jsonString.replace(/'/g, '"');
+                    console.log(jsonString)
                     contentArray = JSON.parse(jsonString);
                     messageContainer.appendChild(textSpan);
                     console.log(contentArray)
@@ -301,7 +351,7 @@ function loadMessages(chatId) {
                     const textSpan = document.createElement('span');
                     textSpan.style.maxWidth = '300px';
                     textSpan.style.wordWrap = 'break-word';
-                    textSpan.textContent = `${message.sender}: ${message.content}`;
+                    textSpan.textContent = `${message.sender_user}: ${message.content}`;
                     messageContainer.appendChild(textSpan);
                 }
             }
@@ -396,20 +446,118 @@ if ('serviceWorker' in navigator) {
   });
 }
 
+function chatUserView(chatId) {
+    console.log(chatId)
+    fetch(`/api/chat/?chat_id=${chatId}`, {
+       method: 'GET',
+       headers: {
+           'Authorization': 'Bearer ' + localStorage.getItem('access_token')
+         },
+    })
+    .then(response => response.json())
+    .then(data => {
+        const userList = document.getElementById('groupUsers');
+        userList.innerHTML = ''; // Очистить список
+        const usersGroup = data.users
 
+        // Добавляем стили для списка
+        userList.style.listStyle = 'none';
+        userList.style.padding = '0';
+        userList.style.margin = '0';
 
-function createChat(senderId, recipientId, type) {
+        usersGroup.forEach(user => {
+            const li = document.createElement('li');
+
+            // Стили для каждого элемента
+            li.style.display = 'flex';
+            li.style.alignItems = 'center';
+            li.style.padding = '12px 16px';
+            li.style.marginBottom = '8px';  // ← Отступ между элементами
+            li.style.borderRadius = '8px';
+            li.style.borderBottom = '1px solid #e0e0e0';  // ← Разделительная линия
+
+            const img = document.createElement('img')
+            img.src = user[1]
+            img.style.width = '40px';  // Увеличил размер
+            img.style.height = '40px';
+            img.style.borderRadius = '50%';  // Круглая аватарка
+            img.style.marginRight = '12px';
+            img.style.objectFit = 'cover';
+
+            const span = document.createElement('span')
+            span.textContent = user[0]; // Имя пользователя
+            span.style.flex = '1';  // Занимает все доступное место
+            span.style.fontSize = '16px';
+            span.style.fontWeight = '500';
+
+            const role = document.createElement('span')
+            role.textContent = user[2];
+            role.style.padding = '4px 12px';
+            role.style.backgroundColor = '#1F9494';
+            role.style.color = 'white';
+            role.style.borderRadius = '20px';
+            role.style.fontSize = '12px';
+            role.style.fontWeight = '600';
+
+            li.appendChild(img)
+            li.appendChild(span)
+            li.appendChild(role)
+            userList.appendChild(li);
+            getCurrentUser().then(currentUser => {
+                if (currentUser.name === user[0] && user[2] === 'Создатель') {
+                    document.querySelector('.edit-group').style.display = 'block'
+                    document.getElementById('photo_group_input').style.display = 'block'
+                    document.getElementById('name_group_input').disabled = 0
+                    document.getElementById('bio_group_input').disabled = 0
+                }
+            })
+        });
+    })
+}
+
+function editChat(chat_id, name, bio, photo, type) {
+   const formData = new FormData();
+   formData.append('chat_id', chat_id);
+   formData.append('name', name);
+   formData.append('bio', bio);
+   if (photo_group_input.files.length > 0) {
+      for (let i = 0; i < photo_group_input.files.length; i++) {
+          console.log(photo_group_input.files[i])
+          formData.append('photo', photo_group_input.files[i]);
+      }
+   }
+   formData.append('type', type);
+   fetch('/api/chat/', { // Эндпоинт для создания нового чата
+       method: 'PUT',
+       headers: {
+           'Authorization': 'Bearer ' + localStorage.getItem('access_token')
+       },
+       body: formData
+   })
+   .then(response => response.json())
+}
+
+function createChat(groupUsers, name, bio, photo, type) {
+   const formData = new FormData();
+   for (let i = 0; i <= groupUsers.length - 1; i++) {
+      formData.append(`users[${i}]`, groupUsers[i]);;
+   }
+
+   formData.append('name', name);
+   formData.append('bio', bio);
+   if (photo_group_input.files.length > 0) {
+      for (let i = 0; i < photo_group_input.files.length; i++) {
+          console.log(photo_group_input.files[i])
+          formData.append('photo', photo_group_input.files[i]);
+      }
+   }
+   formData.append('type', type);
    fetch('/api/chat/', { // Эндпоинт для создания нового чата
        method: 'POST',
        headers: {
-           'Content-Type': 'application/json',
            'Authorization': 'Bearer ' + localStorage.getItem('access_token')
        },
-       body: JSON.stringify({
-           sender_id: senderId,
-           recipient_id: recipientId,
-           type: type
-       })
+       body: formData
    })
    .then(response => response.json())
    .then(data => {
@@ -447,11 +595,9 @@ document.getElementById('sendMessageButton').addEventListener('click', () => {
        }
    }
    // Добавляем остальные параметры
-   formData.append('recipient_id', selectedUserId);
-   console.log(selectedUserId)
    formData.append('chat_id', currentChatId);
    formData.append('type', type);
-   console.log(currentChatId)
+   console.log(type)
    fetch('/api/message/', {
        method: 'POST',
        headers: {
@@ -507,6 +653,7 @@ function displayChats(chats) {
     chatList.innerHTML = ''; // Очистить список
     const numSenderId = senderId
     console.log(chats)
+    chatObj = chats
     chats.forEach(chat => {
         const li = document.createElement('li');
 
@@ -531,19 +678,13 @@ function displayChats(chats) {
                 lis.style.backgroundColor = '#3a3a3a';
                 mediaIcons.style.display = 'none'
             }
-            if (chat.user_id_1 < numSenderId && chat.user_id_2 === numSenderId) {
-                openChat(chat.user_id_1); // Открытие чата при клике
-                li.style.backgroundColor = '#1F9494';
-                selectedUserId = chat.user_id_1
-            } else if (chat.user_id_1 === numSenderId && chat.user_id_2 > numSenderId) {
-                openChat(chat.user_id_2);
-                li.style.backgroundColor = '#1F9494';
-                selectedUserId = chat.user_id_2
-            } else if (chat.user_id_1 === chat.user_id_2 && chat.user_id_1 === numSenderId) {
-                openChat(chat.user_id_1);
-                li.style.backgroundColor = '#1F9494';
-                selectedUserId = chat.user_id_1
-            }
+            currentChatId = chat.id
+            type = chat.type
+            console.log(chat.type)
+            openChat(chat.id);
+            chatUserView(chat.id)
+            li.style.backgroundColor = '#1F9494';
+
         }
         li.appendChild(span);
         li.appendChild(p);
